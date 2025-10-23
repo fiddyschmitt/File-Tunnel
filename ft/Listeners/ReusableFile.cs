ca5644b86b5d22477179328f24e8c4a170f48a2f
Fidel <15338956+fiddyschmitt@users.noreply.github.com>
2025-10-24 09:34:15 +1000

    Reinstate force read in ReceivePump

--- ft/Listeners/ReusableFile.cs
+++ ft/Listeners/ReusableFile.cs
@@ -312,7 +312,7 @@ namespace ft.Listeners
                                 break;
                             }
 
-                            //fileStream.ForceRead(TunnelTimeoutMilliseconds, Verbose);
+                            fileStream.ForceRead(TunnelTimeoutMilliseconds, Verbose);
 
                             if (checkForSessionChange.ElapsedMilliseconds > 1000)
                             {
