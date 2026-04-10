967ca8a2be0c78c8256bd55d13d55ed461662b6e
Fidel <15338956+fiddyschmitt@users.noreply.github.com>
2026-04-10 13:36:48 +1000

    Increment version number

--- ft/Program.cs
+++ ft/Program.cs
@@ -23,7 +23,7 @@ namespace ft
     public class Program
     {
         const string PROGRAM_NAME = "File Tunnel";
-        const string VERSION = "3.0.1";
+        const string VERSION = "3.0.2";
 
         public const int UNIVERSAL_TIMEOUT_MS = 4000;
 
